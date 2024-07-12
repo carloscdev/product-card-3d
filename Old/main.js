2abf2ae038876f949aebed87866dec59b088dc6a
Carlos Córdova <carlos_cdo@outlook.com>
2024-07-11 20:53:07 -0500

    new design and animations

--- Old/main.js
+++ Old/main.js
@@ -14,44 +14,43 @@ container.appendChild(renderer.domElement);
 
 
 const scene = new THREE.Scene();
-scene.rotation.x = 0.1;
 const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 10000);
-camera.position.set(5, 2, 0);
+camera.position.set(5, 2, 2);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 25);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 20);
 directionalLight.position.set(0, 10, 10);
 scene.add(directionalLight);
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableZoom = false;
+controls.enableZoom = true;
 controls.update();
 
 const loadModel = () => {
   const gltfLoader = new GLTFLoader();
   gltfLoader.load('./assets/scene.gltf', (gltf) => {
     const model = gltf.scene;
-    // Escalar el modelo
-    const scaleFactor = 700; // Ajusta este valor según sea necesario
-    model.scale.set(scaleFactor, scaleFactor, scaleFactor);
+        // Escalar el modelo
+        const scaleFactor = 500; // Ajusta este valor según sea necesario
+        model.scale.set(scaleFactor, scaleFactor, scaleFactor);
 
-    // Calcular el centro del modelo
-    const box = new THREE.Box3().setFromObject(model);
-    const center = box.getCenter(new THREE.Vector3());
-    model.position.sub(center); // Mover el modelo al origen
+        // Calcular el centro del modelo
+        const box = new THREE.Box3().setFromObject(model);
+        const center = box.getCenter(new THREE.Vector3());
+        model.position.sub(center); // Mover el modelo al origen
 
-    // Crear un grupo y añadir el modelo
-    // const group = new THREE.Group();
-    // group.add(model);
-    scene.add(model);
+        // Crear un grupo y añadir el modelo
+        const group = new THREE.Group();
+        group.add(model);
+        scene.add(group);
   });
 };
 
 function animate() {
   if (renderer) {
     // rotate y
-    scene.rotation.y += 0.003;
+    scene.rotation.y += 0.01;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
   }
